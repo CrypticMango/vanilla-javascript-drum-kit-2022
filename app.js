114572b804a0d2686d81454148ca9fb1983f2825
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-09-20 22:21:21 -0600

    added simple alert function on click
    -gave buttons id's and classes
    -added readme file
    -added basic body styling

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-//alert("helloooooooo!");
+let button = document.getElementById("drum-pads");
+button.addEventListener("click", buttonPress);
+
+function buttonPress() {
+    alert("teehee you pressed a button 😂");
+}
+
+console.log(button);
