194b6b32c38fb7f42b33e8776e48c4db43f7d2bd
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-09-24 17:00:52 -0600

    added keyup and keydown functions
    -changed color on keyup and keydown

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 //Cymbal button
 const cymbalButton = document.getElementById("cymbal");
 const cymbalSound = new Audio('drum-sounds/cymbal (4).wav');
@@ -8,6 +10,19 @@ function cymbalButtonPress() {
     cymbalSound.play();
     cymbalSound.currentTime = 0;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'q'){
+        cymbalButtonPress();
+        document.getElementById('cymbal').style.backgroundColor = '#C689C6';
+    }
+  });
+
+document.addEventListener('keyup', function(event) {
+    if (event.key === 'q'){
+        document.getElementById('cymbal').style.backgroundColor = '#FFD1D1';
+    }
+  });
 
 //Clap button
 const clapButton = document.getElementById("clap");
@@ -20,6 +35,18 @@ function clapButtonPress() {
     clapSound.currentTime = 0;
 }
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'w'){
+        clapButtonPress();
+        document.getElementById('clap').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'w'){
+        document.getElementById('clap').style.backgroundColor = '#FFD1D1';
+    }
+  });
 //floor tom button
 const floorTomButton = document.getElementById("floor-tom");
 const floorTomSound = new Audio('drum-sounds/floor-tom.wav');
@@ -30,6 +57,19 @@ function floorTomButtonPress() {
     floorTomSound.play();
     floorTomSound.currentTime = 0;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'e'){
+        floorTomButtonPress();
+        document.getElementById('floor-tom').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'e'){
+        document.getElementById('floor-tom').style.backgroundColor = '#FFD1D1';
+    }
+  });
 
 //mid tom button
 const midTomButton = document.getElementById("mid-tom");
@@ -42,6 +82,19 @@ function midTomButtonPress() {
     midTomSound.currentTime = 0;
 }
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'a'){
+        midTomButtonPress();
+        document.getElementById('mid-tom').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'a'){
+        document.getElementById('mid-tom').style.backgroundColor = '#FFD1D1';
+    }
+  });
+
 //high tom button
 const highTomButton = document.getElementById("high-tom");
 const highTomSound = new Audio('drum-sounds/high-tom.wav');
@@ -52,6 +105,19 @@ function highTomButtonPress() {
     highTomSound.play();
     highTomSound.currentTime = 0;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 's'){
+        highTomButtonPress();
+        document.getElementById('high-tom').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 's'){
+        document.getElementById('high-tom').style.backgroundColor = '#FFD1D1';
+    }
+  });
 
 
 //Kick button
@@ -65,6 +131,19 @@ function kickButtonPress() {
     kickSound.currentTime = 0;
 }
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'd'){
+        kickButtonPress();
+        document.getElementById('kick').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'd'){
+        document.getElementById('kick').style.backgroundColor = '#FFD1D1';
+    }
+  });
+
 //snare button
 const snareButton = document.getElementById("snare");
 const snareSound = new Audio('drum-sounds/snare (45).wav');
@@ -75,6 +154,19 @@ function snareButtonPress() {
     snareSound.play();
     snareSound.currentTime = 0;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'z'){
+        snareButtonPress();
+        document.getElementById('snare').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'z'){
+        document.getElementById('snare').style.backgroundColor = '#FFD1D1';
+    }
+  });
 
 //high hat open
 const hiHatOButton = document.getElementById("open-hi");
@@ -87,6 +179,19 @@ function hiHatOButtonPress() {
     hiHatOSound.currentTime = 0;
 }
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'x'){
+        hiHatOButtonPress();
+        document.getElementById('open-hi').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'x'){
+        document.getElementById('open-hi').style.backgroundColor = '#FFD1D1';
+    }
+  });
+
 
 //high hat closed
 const hiHatCButton = document.getElementById("closed-hi");
@@ -98,3 +203,20 @@ function hiHatCButtonPress() {
     hiHatCSound.play();
     hiHatCSound.currentTime = 0;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'c'){
+        hiHatCButtonPress();
+        document.getElementById('closed-hi').style.backgroundColor = '#C689C6';
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    if (event.key === 'c'){
+        document.getElementById('closed-hi').style.backgroundColor = '#FFD1D1';
+    }
+  });
+
+
+
+
